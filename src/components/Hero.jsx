5b8dbdb77ecb59a08864d5924e4c75b180f27a3a
Solomon216 <solomonparthi3@gmail.com>
2024-07-04 21:38:43 +0530

    updtaes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Hero(){
     return(
-        <div className="flex justify-center items-center flex-col mt-12 h-full w-full pb-[400px] max-[430px]:pb-[150px] ">
+        <div className="flex justify-center items-center flex-col mt-12 h-full w-full pb-[400px] pb-[150px] ">
             <p className="text-white text-[50px] max-[435px]:text-[30px]">Hey I'm</p>
             <p className="text-[120px] max-[435px]:text-[70px] prod h-[150] bg-gradient-to-r from-cyan-300 via-purple-400 to to-blue-300 bg-clip-text text-transparent w-fit">Solomon</p>
         </div>
